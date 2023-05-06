462db14b1bdd625bf7706ac0e1ebab4b41aa256e
Mert <mertokan5020@gmail.com>
2023-05-06 16:53:43 +0300

    kart karıştırma yapıldı

--- assets/js/uno.js
+++ assets/js/uno.js
@@ -12,12 +12,48 @@ for (let i = 0; i < 4; i++) {
             number: numbers[j]
         });
     }
+
+
+    //? 0 dan her renkte 1 tane olacak
     for (j = 1; j < 10; j++) {
         deck.push({
             color: colors[i],
             number: numbers[j]
         });
     }
+    //* 0 kart 1 tane eklendi
 }
+// console.table(deck);
 
-console.table(deck)
+
+// TODO: 0 kart kontrolu
+
+// function zero(card) {
+//     return card.number === '0';
+// }
+
+// const zeroCards = deck.filter(zero);
+// console.table(zeroCards);
+
+//* 0 kartlarin kontrolu
+
+
+// TODO: Kart karistirma
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+shuffle(deck);
+
+//* Kartlarin karistirilmasi
+
+
+
+
+console.table(deck);
+
+
